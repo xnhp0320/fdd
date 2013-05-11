939313391db54ad12deff247db1a30d2e28a1539
hepeng <hepeng@localhost.localdomain>
2013-05-11 21:57:43 +0200

    add mem calc method

--- fdd.py
+++ fdd.py
@@ -7,6 +7,10 @@ import re
 import os
 import rule
 
+RULE_INDEX = 2
+RANGE_SIZE = 8
+POINTER_SIZE = 4
+NODE_SIZE = 1
 
 
 class Bitmap:
@@ -60,6 +64,10 @@ class FDD:
     def __init__(self, order):
         self.order = order
         self.root = FDDNode()
+
+        self.nodecnt = 0
+        self.edgecnt = 0
+        self.rangecnt = 0
 
     def build_interval(self, rset):
         rl = list(set(rset))
@@ -138,9 +146,10 @@ class FDD:
         nextlevel = []
         bms = {}
 
+        #nodecnt = 0
+        #edgecnt = 0
+        #rangecnt = 0
         for dim in self.order:
-            nodecnt = 0
-            edgecnt = 0
             for node in thislevel:
                 node.dim = dim
                 rs = [(pc[x])[dim].r for x in node.ppc]
@@ -158,11 +167,12 @@ class FDD:
 
                 for equalrs in bms.values():
                     edge = FDDEdge()
-                    edgecnt += 1
+                    self.edgecnt += 1
                     for ri in equalrs:
                         edge.rangeset.append(i[ri])
+                        self.rangecnt += 1
                     edge.node = FDDNode()
-                    nodecnt += 1
+                    self.nodecnt += 1
                     edge.node.ppc = self.build_node_pc(pc, node, edge)
                     nextlevel.append(edge.node)
                     node.edgeset.append(edge)
@@ -173,11 +183,26 @@ class FDD:
 
 
             del thislevel
-            print "finish", dim
-            print "nodecnt", nodecnt
-            print "edgecnt", edgecnt
+            #print "finish", dim
+            #print "nodecnt", nodecnt
+            #print "edgecnt", edgecnt
             thislevel = nextlevel
             nextlevel = []
+
+    def fdd_mem(self, n):
+        mem = 0
+        mem += len(n.edgeset) * POINTER_SIZE + NODE_SIZE
+
+        if n.dim == -1:
+            mem += len(n.ppc) * RULE_INDEX
+            return mem
+
+        for e in n.edgeset:
+            mem += len(e.rangeset) * RANGE_SIZE
+            mem += self.fdd_mem(e.node)
+
+        return mem
+
 
         #TO: an isomorphic code here
 
@@ -188,7 +213,9 @@ if __name__ == "__main__":
     order=[0,1,2,3,4]
     f = FDD(order)
     #the last one is a wild rule
+
     f.build_fdd(pc)
+    print "FDD(mem):", f.fdd_mem(f.root), "bytes"
 
     #for i in f.root.edgeset:
     #    print i.rangeset
